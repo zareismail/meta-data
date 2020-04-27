ebdb3dc1f597de7ec8c09484ec994fea66de3e5f
اسماعیل زارع <zarehesmaiel@gmail.com>
2020-04-27 18:45:49 +0430

    Loaded migration files

--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -14,6 +14,7 @@ class ToolServiceProvider extends ServiceProvider
      */
     public function boot()
     { 
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     } 
 
     /**
